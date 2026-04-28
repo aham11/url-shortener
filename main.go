a50ba7dbe9b091b81f2b84c8d8511d7a2ce0a450
Anas HAMCHOUZ <shipsperfume5l@gmail.com>
2026-04-28 03:47:10 +0000

    Initialize SQLite database

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"math/rand"
 	"net/http"
 	"sync"
+
+	_ "modernc.org/sqlite"
 )
 
 // URLStore safely manages URLs in memory
@@ -17,6 +20,8 @@ type URLStore struct {
 var store = URLStore{
 	urls: make(map[string]string),
 }
+
+var db *sql.DB
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 const shortCodeLength = 6
@@ -139,17 +144,43 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, originalURL, http.StatusFound)
 }
 
-func main() {
-	
- 	http.HandleFunc("/live", func(w http.ResponseWriter, r *http.Request) {
- 	 w.WriteHeader(http.StatusOK)
-  	w.Write([]byte("alive"))
- 	})
+func initDB() error {
+	var err error
 
- 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-  	 w.WriteHeader(http.StatusOK)
-  	 w.Write([]byte("healthy"))
- 	})
+	db, err = sql.Open("sqlite", "urls.db")
+	if err != nil {
+		return fmt.Errorf("failed to open db: %w", err)
+	}
+
+	query := `
+ 	CREATE TABLE IF NOT EXISTS urls (
+ 	 id INTEGER PRIMARY KEY AUTOINCREMENT,
+  	code TEXT NOT NULL UNIQUE,
+  	original_url TEXT NOT NULL,
+  	created_at TEXT NOT NULL,
+  	visits INTEGER NOT NULL DEFAULT 0
+ 	);`
+
+	if _, err := db.Exec(query); err != nil {
+		return fmt.Errorf("failed to create table: %w", err)
+	}
+
+	return nil
+}
+func main() {
+	if err := initDB(); err != nil {
+		fmt.Printf("DB error: %s\n", err)
+		return
+	}
+	http.HandleFunc("/live", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("alive"))
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("healthy"))
+	})
 	http.HandleFunc("/", handleRoot) // Handles GET /, POST /, and GET /:code
 
 	fmt.Println("Server is running at http://localhost:8080")
